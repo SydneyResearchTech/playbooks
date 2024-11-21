a86f75704536af597d512c6a37f7756500b21973
Dean Taylor <dean.taylor@sydney.edu.au>
2024-11-21 08:16:34 +0800

    move nvidia to role fix dev env dns resolution timing and resilience issues

--- roles/microk8s/files/profile.sh
+++ roles/microk8s/files/profile.sh
@@ -1,10 +1,11 @@
 # Ansible role microk8s
 # src: role/microk8s/files/profile.sh
 if [ -x /snap/bin/microk8s ]; then
-  if ! which kubectl >/dev/null; then
+  if which kubectl >/dev/null; then
+    source <(kubectl completion bash)
+  else
     alias kubectl="microk8s kubectl"
     alias helm="microk8s helm3"
     source <(microk8s kubectl completion bash)
   fi
-  source <(kubectl completion bash)
 fi
